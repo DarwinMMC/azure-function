58d0e6f34e7e7f562d8fe32b9049590f0437849f
Darwin Mercado <56990125+DarwinMMC@users.noreply.github.com>
2026-04-07 20:06:21 -0500

    Update funcionDarwin.js

--- src/functions/funcionDarwin.js
+++ src/functions/funcionDarwin.js
@@ -2,7 +2,7 @@ const { app } = require('@azure/functions');
 
 app.http('funcionDarwin', {
     methods: ['GET', 'POST'],
-    authLevel: 'function',
+    authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
